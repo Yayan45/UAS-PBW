d49ddec838ff577bfa4e4c24144a514cc2f861fc
unknown <yayanrifan45@gmail.com>
2025-01-09 16:06:04 +0700

    menambahkan css dan juga menambahkan waktu di homepage.php

--- homepage.php
+++ homepage.php
@@ -14,7 +14,15 @@ include "koneksi.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
+
+    <!-- CDN BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CDN JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -32,31 +40,13 @@ include "koneksi.php";
         </div>
     </header>
 
-    <!-- <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6">
-                <div class="card shadow-sm">
-                    <div class="card-body">
-                        <h2 class="card-title text-center">Halo, <?php echo $_SESSION['username']; ?>!</h2>
-                        <p class="card-text text-center">Stock Barang Sembako Jaya</p>
-                        
-                        <div class="text-center mb-3">
-                            <p>Username Anda: <strong><?php echo $_SESSION['username']; ?></strong></p>
-                            <p>User ID Anda: <strong><?php echo $_SESSION['userid']; ?></strong></p>
-                        </div>
-
-                        <div class="text-center">
-                            <h5 class="center">
-                                <span id="tanggal"></span> <br>
-                                <span id="jam"></span>
-                            </h5>
-                            <a href="logout.php" class="btn btn-danger btn-lg">Log Out</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-
     <div class="container mt-5">
+        <div class="text-end waktu">
+            <h5 class="center">
+                <span id="tanggal"></span> <br>
+                <span id="jam"></span>
+            </h5>
+        </div>
         <a href="input.php" class="btn btn-success mb-3">Tambah Data</a>
         <a href="logout.php"> <button class="btn btn-danger mb-3">keluar</button></a>
         <?php
@@ -120,6 +110,15 @@ include "koneksi.php";
                 ":" +
                 waktu.getSeconds();
         }
+    </script>
+    <script>
+        $(document).ready(function() {
+
+            var username = "<?php echo $_SESSION['username']; ?>";
+            var userid = "<?php echo $_SESSION['userid']; ?>";
+
+            alert('Selamat Datang ' + username + '');
+        });
     </script>
 </body>
 
